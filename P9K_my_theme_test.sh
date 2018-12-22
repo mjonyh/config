f9c32f2ead142a7916d4d98d5c1bad0982e6f6f3
Enamul <mjonyh@gmail.com>
2018-12-22 20:36:47 +0600

    Configuration file added

---
+++ P9K_my_theme_test.sh
@@ -0,0 +1,4 @@
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon user dir vcs newline)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history ip disk_usage background_jobs load battery)
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+#
