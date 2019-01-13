23ec5b09916c1374af9ef6039803252e5e4631c1
mjonyh <mjonyh@gmail.com>
2019-01-13 21:10:22 +0600

    symlink updated

--- symdotfiles.sh
+++ symdotfiles.sh
@@ -7,9 +7,9 @@
 [ ! -d ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 mv ~/.zshrc ~/.zshrc.bak
-ln -s new_zshrc ~/.zshrc
+ln -s ~/.dotfiles/new_zshrc ~/.zshrc
 mv ~/.vimrc ~/.vimrc.bak
-ln -s vimrc_vundle ~/.vimrc
+ln -s ~/.dotfiles/vimrc_vundle ~/.vimrc
 vim +PluginInstall +qall
 
 cd ~/.vim/bundle/youcompleteme
