bba0508aa5e0c36906755a63a176cfeb9ef3709d
Md. Enamul Hoque <mjonyh@gmail.com>
2019-01-13 18:07:44 +0600

    setup file modified

--- symdotfiles.sh
+++ symdotfiles.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-sh vim_setup.sh
+# Check if bundle directory exists.
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
+
+# Check if Vundle installed
+[ ! -d ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 mv ~/.zshrc ~/.zshrc.bak
 ln -s new_zshrc ~/.zshrc
 mv ~/.vimrc ~/.vimrc.bak
-ln -s vimrc ~/.vimrc
+ln -s vimrc_vundle ~/.vimrc
+vim +PluginInstall +qall
