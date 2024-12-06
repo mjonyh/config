145aa163ec436e619bf323af8f285c17d5ff577d
mjonyh <mjonyh@gmail.com>
2024-12-06 16:16:58 +0600

    note taking script included

---
+++ scripts/daily_notes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ensure the notes directory exists
+NOTES_DIR="$HOME/git/markdown/notes"
+mkdir -p "$NOTES_DIR"
+
+# Generate the filename based on the current date
+FILE_NAME="$(date +'%Y-%m-%d').md"
+noteFilename="$NOTES_DIR/$FILE_NAME"
+
+# Ensure the file exists
+# touch "$noteFilename"
+
+if [ ! -f $noteFilename ]; then
+  echo "# Notes for $(date +%Y-%m-%d)" > $noteFilename
+fi
+
+nvim -c "norm Go" \
+  -c "norm Go## $(date +%H:%M)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $noteFilename
+
+# # Launch a new Kitty terminal
+# kitty --detach bash -c "
+#     tmux new-session -d -s notes_session && \
+#     tmux send-keys 'nvim +startinsert $FILE_PATH' C-m && \
+#     tmux attach-session -t notes_session
+# "
+
