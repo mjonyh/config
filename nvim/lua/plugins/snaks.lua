2d54e420e82fe341f5b47c01c6379047da2c01f0
mjonyh <mjonyh@gmail.com>
2024-12-24 19:21:15 +0600

    notes included

--- nvim/lua/plugins/snaks.lua
+++ nvim/lua/plugins/snaks.lua
@@ -25,7 +25,7 @@ return {
   keys = {
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    -- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
