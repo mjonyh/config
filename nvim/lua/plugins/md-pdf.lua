f6274be20136d88642fef7fd08b6242ea492e2d7
Md Enamul Hoque <mjonyh@Mds-MacBook-Air.local>
2025-09-28 22:48:43 +0600

    📝 Add comprehensive markdown workflow

--- nvim/lua/plugins/md-pdf.lua
+++ nvim/lua/plugins/md-pdf.lua
@@ -1,13 +1,30 @@
 return {
     'arminveres/md-pdf.nvim',
-    branch = 'main', -- you can assume that main is somewhat stable until releases will be made
+    branch = 'main',
+    ft = "markdown",
     lazy = true,
     keys = {
         {
-            "<leader>;",
+            "<leader>mdpdf",
             function() require("md-pdf").convert_md_to_pdf() end,
-            desc = "Markdown preview",
+            desc = "Convert Markdown to PDF",
         },
     },
-    opts = {},
+    config = function()
+      require("md-pdf").setup({
+        -- Set margins (in inches)
+        margins = "1in",
+        -- Set toc (table of contents)
+        toc = true,
+        -- pandoc_user_args can be used as an escape hatch when you want to set
+        -- a pandoc argument that is not represented in the setup opts, it gets
+        -- passed to the end of the pandoc command
+        pandoc_user_args = {
+          "--pdf-engine=xelatex",
+          "--variable=geometry:letterpaper",
+          "--variable=fontsize:11pt",
+          "--highlight-style=github"
+        }
+      })
+    end,
 }
--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,9 +1,65 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+    dependencies = { 
+      'nvim-treesitter/nvim-treesitter', 
+      'nvim-tree/nvim-web-devicons' -- Use consistent icons with your setup
+    },
+    ft = "markdown",
+    keys = {
+      {
+        "<leader>mr",
+        "<cmd>RenderMarkdown toggle<cr>",
+        desc = "Toggle Markdown Rendering",
+      },
+    },
+    config = function()
+      require('render-markdown').setup({
+        -- Heading configurations with beautiful icons
+        headings = {'󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 '},
+        -- Code block styling
+        code = {
+          enabled = true,
+          sign = true,
+          style = 'full', -- Shows language icons and names
+          left_pad = 0,
+          right_pad = 0,
+          width = 'full',
+          border = 'thin',
+        },
+        -- Beautiful bullet points
+        bullet = {
+          enabled = true,
+          icons = { '●', '○', '◆', '◇' },
+          left_pad = 0,
+          right_pad = 0,
+          highlight = 'RenderMarkdownBullet',
+        },
+        -- Enhanced checkbox rendering
+        checkbox = {
+          enabled = true,
+          unchecked = {
+            icon = '󰄱 ',
+            highlight = 'RenderMarkdownUnchecked',
+          },
+          checked = {
+            icon = '󰱒 ',
+            highlight = 'RenderMarkdownChecked',
+          },
+        },
+        -- Table rendering
+        pipe_table = {
+          enabled = true,
+          style = 'full',
+          cell = 'padded',
+        },
+        -- GitHub-style callouts
+        callout = {
+          note = { raw = '[!NOTE]', rendered = '󰋽 Note', highlight = 'RenderMarkdownInfo' },
+          tip = { raw = '[!TIP]', rendered = '󰌶 Tip', highlight = 'RenderMarkdownSuccess' },
+          important = { raw = '[!IMPORTANT]', rendered = '󰅾 Important', highlight = 'RenderMarkdownHint' },
+          warning = { raw = '[!WARNING]', rendered = '󰀪 Warning', highlight = 'RenderMarkdownWarn' },
+          caution = { raw = '[!CAUTION]', rendered = '󰳦 Caution', highlight = 'RenderMarkdownError' },
+        },
+      })
+    end,
 }
